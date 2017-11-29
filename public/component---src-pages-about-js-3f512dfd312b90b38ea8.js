4f142732f9b55eac70d29b84d4afa86326ecc46b
William Owen <william@william-owen.co.uk>
2017-11-29 09:58:35 +0000

    Added build directory

---
+++ public/component---src-pages-about-js-3f512dfd312b90b38ea8.js
@@ -0,0 +1,2 @@
+webpackJsonp([0xefeaa6d1881d],{189:function(e,t,u){"use strict";function a(e){return e&&e.__esModule?e:{default:e}}t.__esModule=!0;var l=u(2),n=a(l),d=u(24),o=a(d),f=function(){return n.default.createElement("div",null,n.default.createElement("h1",null,"About"),n.default.createElement(o.default,{to:"/"},"Go back to the homepage"))};t.default=f,e.exports=t.default}});
+//# sourceMappingURL=component---src-pages-about-js-3f512dfd312b90b38ea8.js.map
